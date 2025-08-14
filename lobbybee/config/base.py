0c0add32c8e8c58212f7b75c2651d9b76b253b10
darkwebplayer <amrithanadhm@gmail.com>
2025-08-14 15:21:52 +0530

    update missing static root

--- lobbybee/config/base.py
+++ lobbybee/config/base.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 import environ
 from pathlib import Path
 
@@ -151,6 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Directory where collectstatic will gather all static files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
