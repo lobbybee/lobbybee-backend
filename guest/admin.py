d77859fc4fa27d6736916ce21c2edb180b3ee137
darkwebplayer <amrithanadhm@gmail.com>
2026-02-20 08:13:10 +0530

    add Feedback to admin panel

--- guest/admin.py
+++ guest/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Guest, GuestIdentityDocument, Stay,Booking
+from .models import Guest, GuestIdentityDocument, Stay,Booking,Feedback
 
 admin.site.register(Guest)
 admin.site.register(GuestIdentityDocument)
 admin.site.register(Stay)
 admin.site.register(Booking)
+admin.site.register(Feedback)
